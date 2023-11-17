cb8b8cd47023d7fd76ef861015f9e0da94969311
kdelange <kimatwork01@gmail.com>
2023-11-17 14:13:13 +0100

    added header and footer for notifications

--- generate_template.sh
+++ generate_template.sh
@@ -66,6 +66,8 @@ bash "${EBROOTMOLGENISMINCOMPUTE}/molgenis_compute.sh" \
 -p "${WORKDIR}/generatedscripts/NGS_Demultiplexing/${RAWDATANAME}/${RAWDATANAME}.csv" \
 -w "${WORKFLOW}" \
 -rundir "${WORKDIR}/runs/NGS_Demultiplexing/${RAWDATANAME}/jobs" \
+--header "${EBROOTNGS_DEMULTIPLEXING}/templates/slurm/header_tnt.ftl" \
+--footer "${EBROOTNGS_DEMULTIPLEXING}/templates/slurm/footer_tnt.ftl" \
 -o "dualBarcode=${dualBarcode};\
 demultiplexingversion=$(module list | grep -o -P 'NGS_Demultiplexing(.+)')" \
 -b slurm \
